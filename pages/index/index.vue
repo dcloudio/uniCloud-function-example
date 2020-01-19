1c87abe69a370cc84ac7b5db39e223757456fa0c
wangyaqi <wangyaqi988@163.com>
2020-01-19 12:17:06 +0800

    update: init project

---
+++ pages/index/index.vue
@@ -0,0 +1,46 @@
+<template>
+	<view class="content">
+		<view class="title">uniCloud 云函数示例</view>
+		<view class="btn-list">
+			<button type="primary" @click="goto('mpweixinLogin')">微信小程序登录</button>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {}
+		},
+		methods: {
+			goto(name) {
+				uni.navigateTo({
+					url: `/pages/${name}/${name}`
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+	.title {
+		font-weight: bold;
+		text-align: center;
+		padding: 20px 0px;
+		font-size: 20px;
+	}
+
+	.tips {
+		color: #999999;
+		font-size: 14px;
+		padding: 20px 30px;
+	}
+
+	.btn-list {
+		padding: 0px 30px;
+	}
+
+	.btn-list button {
+		margin-bottom: 20px;
+	}
+</style>
